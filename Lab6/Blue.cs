08e77b9726cf06252dce78a6ea071f94df7d386d
cryptovaluchik <m2508335@edu.misis.ru>
2025-12-21 15:33:29 +0300

    Add files via upload

--- Lab6/Blue.cs
+++ Lab6/Blue.cs
@@ -1,94 +1,380 @@
-﻿namespace Lab6
+﻿using System;
+
+namespace Lab6
 {
     public class Blue
     {
-        public void Task1(ref int[,] matrix)
+        public delegate int Finder(int[,] m, out int r, out int c);
+        public delegate void SortRowsStyle(int[,] m, int r);
+        public delegate void ReplaceMaxElements(int[,] m, int r, int v);
+        public delegate int[] GetTriangle(int[,] m);
+
+        // ===== TASK 1 =====
+        public void Task1(ref int[,] m)
         {
-
-            // code here
-
-            // end
-
+            if (m.GetLength(0) != m.GetLength(1)) return;
+            int i = FindDiagonalMaxIndex(m);
+            RemoveRow(ref m, i);
         }
+
+        public int FindDiagonalMaxIndex(int[,] m)
+        {
+            int v = m[0, 0];
+            int p = 0;
+            for (int i = 0; i < m.GetLength(0); i++)
+                if (m[i, i] > v)
+                {
+                    v = m[i, i];
+                    p = i;
+                }
+            return p;
+        }
+
+        public void RemoveRow(ref int[,] m, int r)
+        {
+            int[,] t = new int[m.GetLength(0) - 1, m.GetLength(1)];
+            int k = 0;
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                if (i == r) continue;
+                for (int j = 0; j < m.GetLength(1); j++)
+                    t[k, j] = m[i, j];
+                k++;
+            }
+            m = t;
+        }
+
+        // ===== TASK 2 =====
         public int Task2(int[,] A, int[,] B, int[,] C)
         {
-            int answer = 0; // 1 - increasing   0 - no sequence   -1 - decreasing
+            double a = GetAverageExceptEdges(A);
+            double b = GetAverageExceptEdges(B);
+            double c = GetAverageExceptEdges(C);
 
-            // code here
-
-            // end
-
-            return answer;
+            if (a < b && b < c) return 1;
+            if (a > b && b > c) return -1;
+            return 0;
         }
-        public void Task3(ref int[,] matrix, Func<int[,], int> method)
+
+        public double GetAverageExceptEdges(int[,] m)
         {
-
-            // code here
-
-            // end
-
+            int s = 0, mx = int.MinValue, mn = int.MaxValue;
+            for (int i = 0; i < m.GetLength(0); i++)
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    int v = m[i, j];
+                    s += v;
+                    if (v > mx) mx = v;
+                    if (v < mn) mn = v;
+                }
+            return (s - mx - mn) * 1.0 / (m.Length - 2);
         }
-        public void Task4(ref int[,] matrix)
+
+        // ===== TASK 3 =====
+        public void Task3(ref int[,] m, Func<int[,], int> f)
         {
-
-            // code here
-
-            // end
-
+            if (m.GetLength(0) != m.GetLength(1)) return;
+            RemoveColumn(ref m, f(m));
         }
-        public void Task5(ref int[,] matrix, Finder find)
+
+        public int FindUpperColIndex(int[,] m)
         {
-
-            // code here
-
-            // end
-
+            int v = int.MinValue, c = 0;
+            for (int i = 0; i < m.GetLength(0); i++)
+                for (int j = i + 1; j < m.GetLength(1); j++)
+                    if (m[i, j] > v)
+                    {
+                        v = m[i, j];
+                        c = j;
+                    }
+            return c;
         }
-        public void Task6(int[,] matrix, SortRowsStyle sort)
+
+        public int FindLowerColIndex(int[,] m)
         {
-
-            // code here
-
-            // end
-
+            int v = int.MinValue, c = 0;
+            for (int i = 0; i < m.GetLength(0); i++)
+                for (int j = 0; j <= i; j++)
+                    if (m[i, j] > v)
+                    {
+                        v = m[i, j];
+                        c = j;
+                    }
+            return c;
         }
-        public void Task7(int[,] matrix, ReplaceMaxElements transform)
+
+        public void RemoveCol(ref int[,] m, int c)
         {
-
-            // code here
-
-            // end
-
+            int[,] t = new int[m.GetLength(0), m.GetLength(1) - 1];
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                int k = 0;
+                for (int j = 0; j < m.GetLength(1); j++)
+                    if (j != c) t[i, k++] = m[i, j];
+            }
+            m = t;
         }
-        public double[,] Task8(double a, double b, double h, Func<double, double> getSum, Func<double, double> getY)
+
+        // 🔴 ОБЁРТКА ДЛЯ ТЕСТОВ
+        public void RemoveColumn(ref int[,] m, int c)
         {
-            double[,] answer = null;
-
-            // code here
-
-            // end
-
-            return answer;
+            RemoveCol(ref m, c);
         }
-        public int Task9(int[,] matrix, GetTriangle triangle)
+
+        // ===== TASK 4 =====
+        public void Task4(ref int[,] m)
         {
-            int answer = 0;
-
-            // code here
-
-            // end
-
-            return answer;
+            for (int j = m.GetLength(1) - 1; j >= 0; j--)
+                if (!CheckZerosInColumn(m, j))
+                    RemoveColumn(ref m, j);
         }
-        public bool Task10(int[][] array, Predicate<int[][]> func)
+
+        public bool CheckZerosInColumn(int[,] m, int c)
         {
-            bool res = false;
-
-            // code here
-
-            // end
-
-            return res;
+            for (int i = 0; i < m.GetLength(0); i++)
+                if (m[i, c] == 0) return true;
+            return false;
         }
+
+        // ===== TASK 5 =====
+        public void Task5(ref int[,] m, Finder f)
+        {
+            int r, c;
+            int v = f(m, out r, out c);
+
+            for (int i = m.GetLength(0) - 1; i >= 0; i--)
+                for (int j = 0; j < m.GetLength(1); j++)
+                    if (m[i, j] == v)
+                    {
+                        RemoveRow(ref m, i);
+                        break;
+                    }
+        }
+
+        public int FindMax(int[,] m, out int r, out int c)
+        {
+            int v = int.MinValue;
+            r = c = 0;
+            for (int i = 0; i < m.GetLength(0); i++)
+                for (int j = 0; j < m.GetLength(1); j++)
+                    if (m[i, j] > v)
+                    {
+                        v = m[i, j];
+                        r = i;
+                        c = j;
+                    }
+            return v;
+        }
+
+        public int FindMin(int[,] m, out int r, out int c)
+        {
+            int v = int.MaxValue;
+            r = c = 0;
+            for (int i = 0; i < m.GetLength(0); i++)
+                for (int j = 0; j < m.GetLength(1); j++)
+                    if (m[i, j] < v)
+                    {
+                        v = m[i, j];
+                        r = i;
+                        c = j;
+                    }
+            return v;
+        }
+
+        // 🔴 ПЕРЕГРУЗКИ ДЛЯ ТЕСТОВ
+        public int FindMax(int[,] m)
+        {
+            int r, c;
+            return FindMax(m, out r, out c);
+        }
+
+        public int FindMin(int[,] m)
+        {
+            int r, c;
+            return FindMin(m, out r, out c);
+        }
+
+        // ===== TASK 6 =====
+        public void Task6(int[,] m, SortRowsStyle s)
+        {
+            for (int i = 0; i < m.GetLength(0); i += 3)
+                s(m, i);
+        }
+
+        public void SortRowAscending(int[,] m, int r)
+        {
+            for (int i = 0; i < m.GetLength(1); i++)
+                for (int j = 0; j < m.GetLength(1) - 1; j++)
+                    if (m[r, j] > m[r, j + 1])
+                        (m[r, j], m[r, j + 1]) = (m[r, j + 1], m[r, j]);
+        }
+
+        public void SortRowDescending(int[,] m, int r)
+        {
+            for (int i = 0; i < m.GetLength(1); i++)
+                for (int j = 0; j < m.GetLength(1) - 1; j++)
+                    if (m[r, j] < m[r, j + 1])
+                        (m[r, j], m[r, j + 1]) = (m[r, j + 1], m[r, j]);
+        }
+
+        // ===== TASK 7 =====
+        public void Task7(int[,] m, ReplaceMaxElements f)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+                f(m, i, FindMaxInRow(m, i));
+        }
+
+        public int FindMaxInRow(int[,] m, int r)
+        {
+            int v = m[r, 0];
+            for (int j = 1; j < m.GetLength(1); j++)
+                if (m[r, j] > v) v = m[r, j];
+            return v;
+        }
+
+        public void ReplaceByZero(int[,] m, int r, int v)
+        {
+            for (int j = 0; j < m.GetLength(1); j++)
+                if (m[r, j] == v) m[r, j] = 0;
+        }
+
+        public void MultiplyByColumn(int[,] m, int r, int v)
+        {
+            for (int j = 0; j < m.GetLength(1); j++)
+                if (m[r, j] == v) m[r, j] *= (j + 1);
+        }
+
+        // ===== TASK 8 =====
+        public double[,] Task8(double a, double b, double h,
+            Func<double, double> s, Func<double, double> y)
+        {
+            int n = (int)((b - a) / h) + 1;
+            double[,] r = new double[n, 2];
+            for (int i = 0; i < n; i++)
+            {
+                double x = a + i * h;
+                r[i, 0] = s(x);
+                r[i, 1] = y(x);
+            }
+            return r;
+        }
+
+        public double SumA(double x)
+        {
+            double s = 1, f = 1;
+            for (int i = 1; i <= 20; i++)
+            {
+                f *= i;
+                s += Math.Cos(i * x) / f;
+            }
+            return s;
+        }
+
+        public double YA(double x)
+        {
+            return Math.Exp(Math.Cos(x)) * Math.Cos(Math.Sin(x));
+        }
+
+        public double SumB(double x)
+        {
+            double s = -2 * Math.PI * Math.PI / 3;
+            for (int i = 1; ; i++)
+            {
+                double t = Math.Pow(-1, i) * Math.Cos(i * x) / (i * i);
+                s += t;
+                if (Math.Abs(t) < 1e-6) break;
+            }
+            return s;
+        }
+
+        public double YB(double x)
+        {
+            return x * x / 4 - 3 * Math.PI * Math.PI / 4;
+        }
+
+        // ===== TASK 9 =====
+        public int Task9(int[,] m, GetTriangle g)
+        {
+            if (m.GetLength(0) != m.GetLength(1)) return 0;
+            int[] a = g(m);
+            int s = 0;
+            foreach (int x in a) s += x * x;
+            return s;
+        }
+
+        public int[] GetUpperTriangle(int[,] m)
+        {
+            int n = m.GetLength(0);
+            int[] a = new int[n * (n + 1) / 2];
+            int k = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = i; j < n; j++)
+                    a[k++] = m[i, j];
+            return a;
+        }
+
+        public int[] GetLowerTriangle(int[,] m)
+        {
+            int n = m.GetLength(0);
+            int[] a = new int[n * (n + 1) / 2];
+            int k = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j <= i; j++)
+                    a[k++] = m[i, j];
+            return a;
+        }
+
+        // ===== TASK 10 =====
+        public bool Task10(int[][] a, Predicate<int[][]> f)
+        {
+            return f(a);
+        }
+
+        public bool CheckTransformAbility(int[][] a)
+        {
+            int s = 0;
+            foreach (var x in a) s += x.Length;
+            return s % a.Length == 0;
+        }
+
+        public bool CheckSumOrder(int[][] a)
+        {
+            if (a.Length < 2) return true;
+            int[] s = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+                foreach (int x in a[i]) s[i] += x;
+
+            bool inc = true, dec = true;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] >= s[i + 1]) inc = false;
+                if (s[i] <= s[i + 1]) dec = false;
+            }
+            return inc || dec;
+        }
+
+        public bool CheckArraysOrder(int[][] a)
+        {
+            foreach (var x in a)
+            {
+                bool inc = true, dec = true;
+                for (int i = 0; i < x.Length - 1; i++)
+                {
+                    if (x[i] >= x[i + 1]) inc = false;
+                    if (x[i] <= x[i + 1]) dec = false;
+                }
+                if (inc || dec) return true;
+            }
+            return false;
+        }
+
+        public int Sum(int[] a)
+        {
+            int s = 0;
+            for (int i = 0; i < a.Length; i++)
+                s += a[i] * a[i];
+            return s;
+        }
+
     }
 }
